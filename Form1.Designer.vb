97834f220a88dc1839b1eb7302a2a0534f8a748e
Rex-2-ui <rexraymond35@gmail.com>
2026-02-12 11:11:09 +0000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        Label5 = New Label()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        GroupBox2 = New GroupBox()
+        GroupBox3 = New GroupBox()
+        ComputeButton = New Button()
+        ResetButton = New Button()
+        ExitButton = New Button()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        GroupBox1.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Label5)
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Location = New Point(39, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(905, 196)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "GroupBox1"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(411, 150)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(53, 20)
+        Label5.TabIndex = 4
+        Label5.Text = "Label5"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(411, 104)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(53, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "Label4"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(39, 164)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(53, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Label3"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(39, 104)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(53, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Label2"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(39, 42)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(53, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Label1"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Location = New Point(56, 230)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(279, 354)
+        GroupBox2.TabIndex = 1
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "GroupBox2"
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Controls.Add(Label9)
+        GroupBox3.Controls.Add(Label6)
+        GroupBox3.Controls.Add(Label8)
+        GroupBox3.Controls.Add(Label7)
+        GroupBox3.Location = New Point(374, 230)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(375, 241)
+        GroupBox3.TabIndex = 2
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "GroupBox3"
+        ' 
+        ' ComputeButton
+        ' 
+        ComputeButton.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        ComputeButton.Location = New Point(389, 539)
+        ComputeButton.Name = "ComputeButton"
+        ComputeButton.Size = New Size(115, 45)
+        ComputeButton.TabIndex = 3
+        ComputeButton.Text = "Compute"
+        ComputeButton.UseVisualStyleBackColor = True
+        ' 
+        ' ResetButton
+        ' 
+        ResetButton.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        ResetButton.Location = New Point(523, 539)
+        ResetButton.Name = "ResetButton"
+        ResetButton.Size = New Size(103, 45)
+        ResetButton.TabIndex = 4
+        ResetButton.Text = "Reset"
+        ResetButton.UseVisualStyleBackColor = True
+        ' 
+        ' ExitButton
+        ' 
+        ExitButton.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        ExitButton.Location = New Point(653, 539)
+        ExitButton.Name = "ExitButton"
+        ExitButton.Size = New Size(134, 45)
+        ExitButton.TabIndex = 5
+        ExitButton.Text = "Exit"
+        ExitButton.UseVisualStyleBackColor = True
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(45, 39)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(53, 20)
+        Label6.TabIndex = 5
+        Label6.Text = "Label6"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(45, 94)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(53, 20)
+        Label7.TabIndex = 6
+        Label7.Text = "Label7"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(45, 147)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(53, 20)
+        Label8.TabIndex = 7
+        Label8.Text = "Label8"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(45, 196)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(53, 20)
+        Label9.TabIndex = 8
+        Label9.Text = "Label9"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1014, 657)
+        Controls.Add(ExitButton)
+        Controls.Add(ResetButton)
+        Controls.Add(ComputeButton)
+        Controls.Add(GroupBox3)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents ComputeButton As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ResetButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Dim ExitMessage As String ExitMessage = "Are you sure you want to exit?" If MessageBox.Show(ExitMessage, "Exit", MessageBoxButtons.YesNo) = DialogResult.Yes Then Application.Exit() End If
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
